6312437045cb4c3f5f4bd323b0158465117f41bf
Colin Harris <col.w.harris@gmail.com>
2017-02-24 09:36:04 +0800

    Fleshing out some more of the SmartPlug API

--- HS100-iOS/SmartPlug.swift
+++ HS100-iOS/SmartPlug.swift
@@ -26,46 +26,101 @@ class SmartPlug {
         try self.socket?.connect(to: host, port: port)
     }
     
-    func query(request: [String: Any]) throws -> [String: Any] {
-        try socket?.write(from: TPLinkSmartHomeProtocol.encrypt(content: toJsonData(request)!))
-        
-        var response = Data()
-        let _ = try socket?.read(into: &response)
-        
-        return toJsonObject(TPLinkSmartHomeProtocol.decrypt(data: response))! 
-    }
+    lazy var sysInfo: [String: Any]? = { [unowned self] in
+        return self.getSysInfo()
+    }()
     
-    func sysInfo() -> [String: Any] {
+    lazy var model: String? = { [unowned self] in
+        return self.sysInfo?["model"] as? String
+    }()
+    
+    lazy var alias: String? = { [unowned self] in
+        return self.sysInfo?["alias"] as? String
+    }()
+    
+    lazy var mac: String? = { [unowned self] in
+        return self.sysInfo?["mac"] as? String
+    }()
+    
+    lazy var relayState: Int? = { [unowned self] in
+        return self.sysInfo?["relay_state"] as? Int
+    }()
+    
+    lazy var state: Bool? = { [unowned self] in
+        return self.relayState != nil && self.relayState == 1
+    }()
+    
+    lazy var ledOff: Int? = { [unowned self] in
+        return self.sysInfo?["led_off"] as? Int
+    }()
+    
+    lazy var ledState: Bool? = { [unowned self] in
+        return self.ledOff != nil && self.ledOff == 0
+    }()
+    
+    lazy var hardwareInfo: [String: String]? = { [unowned self] in
+        return [
+            "sw_var": self.sysInfo?["sw_ver"] as! String,
+            "hw_ver": self.sysInfo?["hw_ver"] as! String,
+            "mac": self.sysInfo?["mac"] as! String,
+            "hwId": self.sysInfo?["hwId"] as! String,
+            "fwId": self.sysInfo?["fwId"] as! String,
+            "oemId": self.sysInfo?["oemId"] as! String,
+            "dev_name": self.sysInfo?["dev_name"] as! String
+        ]
+    }()
+    
+    func query(target: String, command: String, args: [String: Any] = [:]) -> [String: Any]? {
         do
         {
-            let request = ["system": ["get_sysinfo": []]]
-            return try query(request: request)
+            try connect()
+            let request = [target: [command: args]]
+            let encryptedRequest = TPLinkSmartHomeProtocol.encrypt(content: SmartPlugJSON.toJson(request)!)
+            try socket?.write(from: encryptedRequest)
+            
+            var data = Data()
+            let _ = try socket?.read(into: &data)
+            
+            let response = SmartPlugJSON.fromJson(TPLinkSmartHomeProtocol.decrypt(data: data))
+            let targetResponse = response?[target] as? [String: Any]
+            let commandResponse = targetResponse?[command] as? [String: Any]
+            return commandResponse
         } catch {
             print("Exception: \(error.localizedDescription)")
-            return ["Error": "error"]
-        }
-    }
-    
-    func toJsonData(_ content: [String: Any]) -> String? {
-        do
-        {
-            let jsonData = try JSONSerialization.data(withJSONObject: content, options: .prettyPrinted)
-            return String(data: jsonData, encoding: String.Encoding.ascii)
-        } catch {
-            print("Json Encode Error: \(error.localizedDescription)")
             return nil
         }
     }
     
-    func toJsonObject(_ content: String) -> [String: Any]? {
-        do
-        {
-            let jsonData = try JSONSerialization.jsonObject(with: content.data(using: String.Encoding.ascii)!, options: .allowFragments)
-            return jsonData as? [String: Any]
-        } catch {
-            print("Json Decode Error: \(error.localizedDescription)")
-            return nil
-        }
+    func getSysInfo() -> [String: Any]? {
+        return query(target: "system", command: "get_sysinfo")
+    }
+    
+    func getTime() -> Date? {
+        let response = query(target: "time", command: "get_time")
+        print("get_time response = \(response)")
+        let cal = Calendar(identifier: .gregorian)
+        var components = DateComponents()
+        components.year = response?["year"] as? Int
+        components.month = response?["month"] as? Int
+        components.day = response?["mday"] as? Int
+        components.hour = response?["hour"] as? Int
+        components.minute = response?["min"] as? Int
+        components.second = response?["sec"] as? Int
+        return cal.date(from: components)
+    }
+    
+    func getTimeZone() -> String? {
+        let response = query(target: "time", command: "get_timezone")
+        print("get_timezone response = \(response)")
+        return "unknown"
+    }
+    
+    func setState(state: Bool) {
+        let _ = query(target: "system", command: "set_relay_state", args: ["state": state ? 1 : 0])
+    }
+    
+    func setLedState(state: Bool) {
+        let _ = query(target: "system", command: "set_led_off", args: ["off": state ? 0 : 1])
     }
     
 }
--- HS100-iOS/ViewController.swift
+++ HS100-iOS/ViewController.swift
@@ -11,22 +11,43 @@ import Socket
 
 class ViewController: UIViewController {
     
+    @IBOutlet var modelField: UITextField!
+    @IBOutlet var aliasField: UITextField!
+    @IBOutlet var macField: UITextField!
+    @IBOutlet var stateSwitch: UISwitch!
+    @IBOutlet var ledStateSwitch: UISwitch!
+    @IBOutlet var hardwareInfo: UITextView!
+    
+    var smartPlug: SmartPlug!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-   
-    @IBAction
-    func buttonPressed() {
-        print("buttonPressed")
-        do {
-            let smartPlug = SmartPlug(host: "192.168.0.162")
-            try smartPlug.connect()
-            let sysInfo = smartPlug.sysInfo()
-            print("SysInfo: \(sysInfo)")
-        } catch {
-            print("Exception \(error.localizedDescription)")
-        }
+        smartPlug = SmartPlug(host: "192.168.0.162")        
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        modelField.text = smartPlug.model
+        aliasField.text = smartPlug.alias
+        macField.text = smartPlug.mac
+        hardwareInfo.text = SmartPlugJSON.toJson(smartPlug.hardwareInfo ?? [:])
+        stateSwitch.setOn(smartPlug.state ?? false, animated: false)
+        ledStateSwitch.setOn(smartPlug.ledState ?? false, animated: false)
+        print("Device Date = \(smartPlug.getTime())")
+        print("Device TimeZone = \(smartPlug.getTimeZone())")
+        
+    }
+    
+    @IBAction
+    func stateSwitchChanged() {
+        print("stateSwitchChanged")
+        smartPlug.setState(state: stateSwitch.isOn)
+    }
+    
+    @IBAction
+    func ledStateSwitchChanged() {
+        print("ledStateSwitchChanged")
+        smartPlug.setLedState(state: ledStateSwitch.isOn)
+    }
 }
 
